d9b8af43b84ce20d594a2ad0980baf68d69ceac3
frju365 <win10@tutanota.com>
2017-07-26 21:57:58 +0200

    Create config.php

---
+++ conf/config.php
@@ -0,0 +1,11 @@
+<?php
+
+    $config = [
+                "base_url"      =>  "https://__URL__",
+                "notes_folder"  =>  "notes/", #do not forget the / at the end!
+                "site_title"  =>  "SiMPad ~ The easy way to take notes",
+                "google_font" => "true",
+                "google_font_title" => "Ubuntu",
+                "encryption" =>  "false",
+                "encrypt_method" => "aes-256-cbc",
+    ];
